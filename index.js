1870d616c9616fd0a44d78889348d86e42c0addb
Rottschaefer <rottschaefer54@gmail.com>
2022-12-08 20:02:36 -0300

    Semana 3 feita

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ const comida1 = {
 
     ingredientes: ["Creme de Leite","Frango","Batata Palha"]
 }
-
 const comida2 = {
     nome: "Bolo de chocolate",
     custoIngrediente: 15,
     temLactose: true,
     ingredientes: ["Leite","Ovos","Chocolate"]
 }
-
-
 const comida3 = {
     nome: "Bolo de banana",
     custoIngrediente: 20,
@@ -25,7 +22,41 @@ const comida3 = {
     ingredientes: ["Banana","açúcar","farinha"]
 }
 
-let arr = []
+let arr = [comida1,comida2,comida3]
+
+// função que imprime o relatório
+ 
+function relatorio(objeto){
+for (const i in objeto) {
+   console.log(`${i}:${objeto[i]}`)
+}
+return ("----------------------") 
+}
+
+// 4. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
+
+function nomeIgualString(arrayObjetos,string){
+    let newArray = arrayObjetos.filter((elemento)=>elemento.nome===string).slice()
+
+    if(newArray.length === 0){
+        alert("Nenhum item foi encontrado")
+    }
+    
+    return newArray
+}
+
+
+
+
+
+
+
+
+// console.log(nome1.toUpperCase(),"\nCusto:", custoIngrediente1, "\nTem Lactose", temLactose1,"\nIngredientes:", ingredientes1)
+// console.log(nome2.toUpperCase(),"\nCusto:", custoIngrediente2, "\nTem Lactose", temLactose2,"\nIngredientes:", ingredientes2)
+// console.log(nome3.toUpperCase(),"\nCusto:", custoIngrediente3, "\nTem Lactose", temLactose3,"\nIngredientes:", ingredientes3)
+
+
 
 // if(comida1.temLactose){
 //     arr.push(comida1)
@@ -47,14 +78,14 @@ let arr = []
 
 
 //Tentativa de fazer o processo com for 
-let comida = `comida${i}`
+// let comida = `comida${i}`
 
-for(let i in comida){
-    console.log(comida)
-if(i == temLactose){
-    arr.push((comida+i))
-}
-else{alert(`${comida} não foi adicionado`)}}
+// for(let i in comida){
+//     console.log(comida)
+// if(i == temLactose){
+//     arr.push((comida+j))
+// }
+// else{alert(`${comida} não foi adicionado`)}}
 
 // Cálculo da média e verificação de se todos os valores booleanos são true
 
@@ -71,3 +102,12 @@ else{alert(`${comida} não foi adicionado`)}}
 // console.log(nome1.toUpperCase(),"\nCusto:", custoIngrediente1, "\nTem Lactose", temLactose1,"\nIngredientes:", ingredientes1)
 // console.log(nome2.toUpperCase(),"\nCusto:", custoIngrediente2, "\nTem Lactose", temLactose2,"\nIngredientes:", ingredientes2)
 // console.log(nome3.toUpperCase(),"\nCusto:", custoIngrediente3, "\nTem Lactose", temLactose3,"\nIngredientes:", ingredientes3)
+
+// Semana 3
+// for (const comida of arr) {
+    //     let texto = ""
+    //     for (const ingrediente of comida.ingredientes) {
+    //         texto += ingrediente + ", "
+    //         comida.ingredientes = texto 
+    //         }
+    // }
